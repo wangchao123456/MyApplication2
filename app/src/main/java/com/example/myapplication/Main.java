023ae4d13787667da70d6a667da94752eac04791
Your Name <you@example.com>
2019-09-13 11:04:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Main.java
@@ -0,0 +1,41 @@
+package com.example.myapplication;
+
+import androidx.annotation.Keep;
+
+/**
+ * @ProjectName: MyApplication2
+ * @Package: com.example.myapplication
+ * @ClassName: Main
+ * @Description: java类作用描述
+ * @Author: wangchao
+ * @CreateDate: 2019/9/13 10:19
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2019/9/13 10:19
+ * @UpdateRemark: 更新说明
+ */
+
+public class Main {
+
+    private String msg;
+
+    public void test(String name){
+        System.out.println("========================");
+        msg= "1231231";
+    }
+
+    public void test1(){
+        System.out.println("========================");
+        msg= "adnroid";
+    }
+
+    public void test2(){
+        System.out.println("========================");
+        msg= "1231231";
+    }
+
+    public void test3(){
+        System.out.println("========================");
+        msg= "kjadshfkjasdf";
+    }
+
+}
